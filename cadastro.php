3440317ba49afd5369434c8075f6b8692394009c
Joaofury <joaovictormorenos123@gmail.com>
2022-04-13 01:37:41 -0300

    adicionado campo cpf

--- cadastro.php
+++ cadastro.php
@@ -1,19 +1,16 @@
-         
 <?php
 	include_once('conexao.php');
 	// recuperando 
 	$email = $_POST['email'];
 	$usuario = $_POST['usuario'];		
 	$senha = md5($_POST['senha']);
+	$cpf = $_POST['cpf'];
 	// criando a linha de INSERT
-	$sqlinsert =  "insert into tablogin ( email,usuario, senha) values ('$email','$usuario', '$senha')";
+	$sqlinsert =  "insert into tablogin ( email,usuario, senha, cpf) values ('$email','$usuario', '$senha', '$cpf')";
 	
 	// executando instrução SQL
  $resultado = @mysqli_query($conn, $sqlinsert);
-//$resultado = @mysqli_query($conexao, "insert into tabelaimg ( codigo,produto, descricao, data, valor) values ('$codigo','$produto', '$descricao', '$data', $valor)");
-if ($senha<8){ //Se X enviado for maior que 3, da erro.Troque o 3 pelo numero que quiser!
-	echo "senha curta";
-	}
+
 	if (!$resultado) {
 		die('Query Inválida: ' . @mysqli_error($conn));  
 	} else {
@@ -22,6 +19,9 @@ if ($senha<8){ //Se X enviado for maior que 3, da erro.Troque o 3 pelo numero qu
 			  window.location.replace('login.html');
 			  </script>;";
 	} 
+
+
+		
 	
 	mysqli_close($conn);
 ?>
