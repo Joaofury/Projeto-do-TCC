d6d385dc567c03ac8c955ce4c9541a2903b06bdd
Joaofury <joaovictormorenos123@gmail.com>
2022-05-09 17:07:59 -0300

    Atualizações visuais

---
+++ js/selectioncustom.js
@@ -0,0 +1,16 @@
+function show(anything){
+                document.querySelector('.text').value = anything;
+              }
+               
+
+              const dropdownf = document.querySelector(".dropdownf");
+const option = document.querySelector(".option");
+const textBox = document.querySelector(".textBox");
+dropdownf.addEventListener("click", () => {
+  dropdownf.classList.toggle("active");
+});
+option.addEventListener("click"), (e) =>
+  console.log(e.target.dataset.value);
+  if (e.target.dataset.identificador === "option") {
+    textBox.value = e.target.innerHTML;
+  }
